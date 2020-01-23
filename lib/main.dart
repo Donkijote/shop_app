5d14060e8c0670e615cb46342721c2372b1a7697
= <=>
2020-01-23 20:58:06 -0300

    adding multi providers

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import './screens/products.dart';
 import './screens/product_detail.dart';
 
+import './providers/cart.dart';
 import './providers/products.dart';
 
 void main() => runApp(MyApp());
@@ -11,8 +12,15 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (ctx) => Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: Products(),
+        ),
+        ChangeNotifierProvider.value(
+          value: Cart(),
+        ),
+      ],
       child: MaterialApp(
         title: 'Shop App',
         theme: ThemeData(
