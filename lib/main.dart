a6e9c4a405dc9339d15e4510ae5388412aabf7c4
Manuel Gonçalves <manuel_11_94@hotmail.es>
2020-01-23 16:32:20 -0300

    adding provider & listener

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import './screens/products.dart';
 import './screens/product_detail.dart';
+
+import './providers/products.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Shop App',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepPurple,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'Shop App',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepPurple,
+          fontFamily: 'Lato',
+        ),
+        home: ProductsScreen(),
+        routes: {
+          ProductDetailScreen.nameRoute: (ctx) => ProductDetailScreen(),
+        },
       ),
-      home: ProductsScreen(),
-      routes: {
-        ProductDetailScreen.nameRoute: (ctx) => ProductDetailScreen(),
-      },
     );
   }
 }
---
+++ lib/widgets/product_grid.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
+import './product_item.dart';
+
+class ProductsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    final products = productsData.item;
+    return GridView.builder(
+      padding: const EdgeInsets.all(10.0),
+      itemCount: products.length,
+      itemBuilder: (ctx, index) {
+        return ProductItem(products[index].id, products[index].title,
+            products[index].imageUrl);
+      },
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10),
+    );
+  }
+}
