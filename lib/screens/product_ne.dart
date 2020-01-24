d1552d275e847f853b27e6fc2e83afe8d1951a5d
Manuel Gonçalves <manuel_11_94@hotmail.es>
2020-01-24 15:50:02 -0300

    new product screen added

---
+++ lib/screens/product_ne.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ProductNEScreen extends StatefulWidget {
+  @override
+  _ProductNEScreenState createState() => _ProductNEScreenState();
+}
+
+class _ProductNEScreenState extends State<ProductNEScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
