7c42f6d090d706f9c43a21e1e05ac03f53eeb2ab
Manuel Gonçalves <manuel_11_94@hotmail.es>
2020-01-24 11:50:16 -0300

    adding prodcut detail page

--- lib/screens/product_detail.dart
+++ lib/screens/product_detail.dart
@@ -16,6 +16,39 @@ class ProductDetailScreen extends StatelessWidget {
           product.title,
         ),
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              '\$${product.price}',
+              style: TextStyle(color: Colors.grey, fontSize: 20),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                product.description,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
