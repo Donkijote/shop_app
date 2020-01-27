55503b437b8c2f6b6944ec3c01ac67b5b151d172
Manuel Gonçalves <manuel_11_94@hotmail.es>
2020-01-27 14:26:57 -0300

    adding order to database

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import '../models/cart.dart';
 import '../models/orders.dart';
@@ -10,13 +13,30 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    const url = 'https://shopstore-64cc5.firebaseio.com/orders.json';
+    final time = DateTime.now();
+    final response = await http.post(
+      url,
+      body: json.encode({
+        'amount': total,
+        'dateTime': time.toIso8601String(),
+        'products': cartProducts
+            .map((item) => {
+                  'id': item.id,
+                  'title': item.title,
+                  'quantity': item.quantity,
+                  'price': item.price,
+                })
+            .toList(),
+      }),
+    );
     _orders.insert(
       0,
       OrderItem(
-          id: DateTime.now().toString(),
+          id: json.decode(response.body)['name'],
           amount: total,
-          dateTime: DateTime.now(),
+          dateTime: time,
           products: cartProducts),
     );
     notifyListeners();
