4e46aec46beff57cffe087dbb68708bee0e35ca2
Manuel Gonçalves <manuel_11_94@hotmail.es>
2020-01-27 14:12:10 -0300

    updating favorites to database

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,8 +20,28 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toogleFavoriteStatus() {
+  void _setFavValue(bool status) {
+    isFavorite = status;
+    notifyListeners();
+  }
+
+  Future<void> toogleFavoriteStatus() async {
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+    final url = 'https://shopstore-64cc5.firebaseio.com/products/$id.json';
+    try {
+      final response = await http.patch(
+        url,
+        body: json.encode({
+          'isFavorite': isFavorite,
+        }),
+      );
+      if (response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+    } catch (e) {
+      _setFavValue(oldStatus);
+    }
   }
 }
